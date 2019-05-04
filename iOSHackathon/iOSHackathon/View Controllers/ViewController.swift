66e9a51717e11cd9ca6c3299d9bbe265ce12e36c
Xu Mo <xumo95@gwu.edu>
2019-05-03 22:06:59 -0400

    Add starter project

---
+++ iOSHackathon/iOSHackathon/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSHackathon
+//
+//  Created by Xu Mo on 5/3/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
