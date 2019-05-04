573478275eb72626129a8fe35185e93b3316d126
Daniela Parra <dyparra47@gmail.com>
2019-05-04 02:06:51 -0400

    Add all VCs and their storyboard scenes

--- iOSHackathon/iOSHackathon/Models/Business.swift
+++ iOSHackathon/iOSHackathon/Models/Business.swift
@@ -10,10 +10,10 @@ import Foundation
 class Business{
     let name: String
     let phoneNumber: String
-    let address: String?
+    let address: String
     let openStatus: Bool
     let owner: String
-    init(name: String, phoneNumber: String, address: String?, openStatus: Bool, owner: String) {
+    init(name: String, phoneNumber: String, address: String, openStatus: Bool, owner: String) {
         self.name = name
         self.phoneNumber = phoneNumber
         self.address = address
