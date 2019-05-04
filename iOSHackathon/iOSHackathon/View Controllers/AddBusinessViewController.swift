4896d70232883ab657ca08cf9eb6e469050f768a
Xu Mo <xumo95@gwu.edu>
2019-05-04 00:35:01 -0400

    Add VC and new file

---
+++ iOSHackathon/iOSHackathon/View Controllers/AddBusinessViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddBusinessViewController.swift
+//  iOSHackathon
+//
+//  Created by Xu Mo on 5/4/19.
+//
+
+import Foundation
+import UIKit
+
+class Business{
+    let name: String
+    init (name: String?){
+        self.name = name // it has a chance so its value can be set!
+    }
+    let phoneNumber: String
+}
