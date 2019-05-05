e73a4bb91de861b05a59e93f9493229ee24a8d14
Xu Mo <xumo95@gwu.edu>
2019-05-05 02:24:17 -0400

    resolve conflicts

--- iOSHackathon/iOSHackathon/View Controllers/AddEditBusinessOfferViewController.swift
+++ iOSHackathon/iOSHackathon/View Controllers/AddEditBusinessOfferViewController.swift
@@ -12,40 +12,66 @@ class AddEditBusinessOfferViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        updateViews()
-    }
-    
-    @IBAction func addUpdateOffer(_ sender: Any) {
-        
-        guard let offerTitle = offerTitleTextField.text,
-            let offerDescription = offerDescriptionTextView.text else { return}
-        
-        if let offer = offer {
-            // UPDATE REQUEST
-            postOffer(title: offerTitle, description: offerDescription, id: offer.offerId)
-            
-        } else {
-            // POST REQUEST
-            postOffer(title: offerTitle, description: offerDescription)
-        }
+        //updateViews()
     }
     
     @IBAction func goBack(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    private func updateViews() {
-        guard let offer = offer else {
-            return
+    @IBOutlet weak var offerTitleTextField: UITextField!
+    @IBOutlet weak var offerDescriptionTextView: UITextView!
+    @IBOutlet weak var offerMerchantNameTextField: UITextField!
+    @IBOutlet weak var offerLocationTextField: UITextField!
+    @IBOutlet weak var offerRedemptionCodeTextField: UITextField!
+    @IBOutlet weak var offerShareTitleTextField: UITextField!
+    @IBOutlet weak var offerStartDatePicker: UIDatePicker!
+    @IBOutlet weak var offerEndDatePicker: UIDatePicker!
+    @IBOutlet weak var offerActiveIndicator: UISwitch!
+    
+    @IBAction func addUpdateOffer(_ sender: Any) {
+        guard let offTitle = offerTitleTextField.text,
+        let offDescription = offerDescriptionTextView.text,
+        let offMerchantName = offerMerchantNameTextField.text,
+        let offLocation = offerLocationTextField.text,
+        let offRedemptionCode = offerRedemptionCodeTextField.text,
+        let offShareTitle = offerShareTitleTextField.text else { return}
+        
+        let offStartDate = offerStartDatePicker.date
+        let offEndDate = offerEndDatePicker.date
+        let offActiveIndicator = offerActiveIndicator.isOn
+        let id = UUID()
+        
+        if offer != nil {
+            // UPDATE REQUEST
+            
+            
+        } else {
+            // POST REQUEST
+            postOffer(title: offTitle, description: offDescription, id: id, merchantName: offMerchantName, location: offLocation, redemptionCode: offRedemptionCode, shareTitle: offShareTitle, activeIndicator: offActiveIndicator, startDate: offStartDate, endDate: offEndDate)
         }
-        
-        offerTitleTextField.text = offer.offerTitle
-        offerDescriptionTextView.text = "\(offer.offerShortDescription)"
-        
     }
     
-    private func postOffer(title: String, description: String, id: Int = UUID().hashValue) {
-        let offer = Offer(title: title, description: description, id: id)
+    
+    
+    
+    
+    private func updateViews() {
+        guard let offer = offer else { return}
+        
+        offerTitleTextField.text = offer.offerTitle
+        offerDescriptionTextView.text = offer.offerShortDescription.text
+        offerMerchantNameTextField.text = offer.merchantList.first?.merchant
+        offerLocationTextField.text = offer.merchantList.first?.merchantAddress.first
+        offerRedemptionCodeTextField.text = offer.redemptionCode
+        offerShareTitleTextField.text = offer.shareTitle
+        //offerStartDatePicker.date = offer.validityFromDate
+        //offerEndDatePicker.date = offer.validityToDate
+        offerActiveIndicator.isOn = offer.activeIndicator
+    }
+    
+    private func postOffer(title: String, description: String, id: Int = UUID().hashValue, merchantName: String, location: String, redemptionCode: String, shareTitle: String, activeIndicator: Bool, startDate: Date, endDate: Date) {
+        let offer = Offer(title: title, description: description, id: id, merchantName: merchantName, merchantLocation: location, redemptionCode: redemptionCode, shareTitle: shareTitle, activeIndicator: activeIndicator, startDate: startDate, endDate: endDate)
         
         let url = URL(string: "https://smallbusinesshackathon.firebaseio.com/offers")!.appendingPathComponent("\(offer.offerId)").appendingPathExtension("json")
         
@@ -85,7 +111,9 @@ class AddEditBusinessOfferViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var offerTitleTextField: UITextField!
-    @IBOutlet weak var offerDescriptionTextView: UITextView!
+    
     
 }
+
+
+
