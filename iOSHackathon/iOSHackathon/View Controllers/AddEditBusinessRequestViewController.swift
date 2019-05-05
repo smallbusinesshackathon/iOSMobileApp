b90168560f928ab3f0775ec86eea202a1d29759d
Daniela Parra <dyparra47@gmail.com>
2019-05-05 00:17:33 -0400

    Add add request styling

--- iOSHackathon/iOSHackathon/View Controllers/AddEditBusinessRequestViewController.swift
+++ iOSHackathon/iOSHackathon/View Controllers/AddEditBusinessRequestViewController.swift
@@ -11,6 +11,13 @@ class AddEditBusinessRequestViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        requestDescriptionTextView.backgroundColor = .clear
+        requestDescriptionTextView.layer.cornerRadius = 5
+        requestDescriptionTextView.layer.borderColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        requestDescriptionTextView.layer.borderWidth = 1
+        
+        submitButton.layer.cornerRadius = 5
 
     }
     @IBAction func goBack(_ sender: Any) {
@@ -98,5 +105,6 @@ class AddEditBusinessRequestViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var submitButton: UIButton!
     
 }
